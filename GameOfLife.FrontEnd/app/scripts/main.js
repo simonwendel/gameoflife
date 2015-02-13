99856bb089e722ce13997bb894bc7509aa9608b8
Simon Wendel <mail@simonwendel.se>
2015-02-13 13:14:46 +0100

    Add html and scripts from old solution to the new

--- GameOfLife.FrontEnd/app/scripts/main.js
+++ GameOfLife.FrontEnd/app/scripts/main.js
@@ -1,2 +1,31 @@
-/* jshint devel:true */
-console.log('\'Allo \'Allo!');
+// <copyright file="gameoflife.js" company="N/A">
+//      Copyright (C) Simon Wendel 2013-2015.
+// </copyright>
+
+(function($) {
+    $(function() {
+        $('#dialog').dialog({
+            modal: true,
+            title: 'Game Of Life',
+            closeOnEscape: false,
+            dialogClass: 'no-close',
+            buttons: [{
+                text: 'Run game',
+                click: function() {
+                    var options = $('#options :input').serialize();
+                    runGame(options, '#output');
+                }
+            }]
+        });
+    });
+
+    function runGame(options, outputSelector) {
+        $.post(
+            'Home/RunGame',
+            options,
+            function(data) {
+                var output = $(outputSelector).empty();
+                output.append(data);
+            });
+    }
+})(jQuery);
