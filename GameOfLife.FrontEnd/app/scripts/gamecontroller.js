ea7356fbb11bb98ea6172a75040cb100c69d1257
Simon Wendel <mail@simonwendel.se>
2015-02-13 18:55:16 +0100

    Move old jQuery logic into GameController for now

--- GameOfLife.FrontEnd/app/scripts/gamecontroller.js
+++ GameOfLife.FrontEnd/app/scripts/gamecontroller.js
@@ -1,4 +1,4 @@
-;(function() {
+;(function($) {
     'use strict';
 
     angular
@@ -12,21 +12,47 @@
         vm.numberOfGenerations = 30;
 
         vm.rules = [
-            { value: 0, name: 'Standard'},
-            { value: 1, name: 'Sierpinskish'}
+            {value: 0, name: 'Standard'},
+            {value: 1, name: 'Sierpinskish'}
         ];
 
         vm.selectedRule = vm.rules[0];
 
         vm.lifeForms = [
-            { value: 0, name: 'Empty' },
-            { value: 1, name: 'Acorn' },
-            { value: 2, name: 'AircraftCarrier' },
-            { value: 3, name: 'FivePoint' },
-            { value: 4, name: 'RandomPattern' }
+            {value: 0, name: 'Empty'},
+            {value: 1, name: 'Acorn'},
+            {value: 2, name: 'AircraftCarrier'},
+            {value: 3, name: 'FivePoint'},
+            {value: 4, name: 'RandomPattern'}
         ];
 
         vm.selectedLifeForm =
             vm.lifeForms[vm.lifeForms.length - 1];
     }
-})();
+
+    function runGame(options, outputSelector) {
+        $.post(
+            'Home/RunGame',
+            options,
+            function(data) {
+                var output = $(outputSelector).empty();
+                output.append(data);
+            });
+    }
+
+    $(function() {
+        $('#dialog').dialog({
+            modal: true,
+            title: 'Game Of Life',
+            closeOnEscape: false,
+            dialogClass: 'no-close',
+            buttons: [{
+                text: 'Run game',
+                click: function() {
+                    var options = $('#options :input').serialize();
+                    runGame(options, '#output');
+                }
+            }]
+        });
+    });
+})(jQuery);
