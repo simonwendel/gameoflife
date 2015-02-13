82fd088ad1360d58dbe963cef74685fe890e6dc6
Simon Wendel <mail@simonwendel.se>
2015-02-13 20:53:56 +0100

    Add a game service that returns some initial game settings

--- GameOfLife.FrontEnd/app/scripts/sw-game-start-dialog.js
+++ GameOfLife.FrontEnd/app/scripts/sw-game-start-dialog.js
@@ -36,29 +36,15 @@
     }
 
     /** @ngInject */
-    function DialogController() {
-        var vm = this;
+    function DialogController(game) {
+        var vm = this,
+            initSettings = game.getInitialSettings();
 
-        vm.numberOfGenerations = 30;
-
-        vm.rules = [
-            {value: 0, name: 'Standard'},
-            {value: 1, name: 'Sierpinskish'}
-        ];
-
-        vm.selectedRule = vm.rules[0];
-
-        vm.lifeForms = [
-            {value: 0, name: 'Empty'},
-            {value: 1, name: 'Acorn'},
-            {value: 2, name: 'AircraftCarrier'},
-            {value: 3, name: 'FivePoint'},
-            {value: 4, name: 'RandomPattern'}
-        ];
-
-        vm.selectedLifeForm =
-            vm.lifeForms[vm.lifeForms.length - 1];
-
+        vm.numberOfGenerations = initSettings.numberOfGenerations;
+        vm.rules = initSettings.rules;
+        vm.selectedRule = initSettings.selectedRule;
+        vm.lifeForms = initSettings.lifeForms;
+        vm.selectedLifeForm = initSettings.selectedLifeForm;
         vm.runGame = runGame;
     }
 
