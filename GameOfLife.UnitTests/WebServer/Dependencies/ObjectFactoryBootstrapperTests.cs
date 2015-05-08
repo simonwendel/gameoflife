e1003592b4334fa2ecce102de72ceb4b102f460a
Simon Wendel <mail@simonwendel.se>
2015-05-08 17:25:33 +0200

    Remove old bootstrapper and introduce new one
    The ResolverBootstrapper is replaced with a new one using the
    IObjectFactory to bootstrap. The new bootstrapper is simpler.

---
+++ GameOfLife.UnitTests/WebServer/Dependencies/ObjectFactoryBootstrapperTests.cs
@@ -0,0 +1,85 @@
+﻿// <copyright file="ObjectFactoryBootstrapperTests.cs" company="N/A">
+//      Copyright (C) Simon Wendel 2013-2015.
+// </copyright>
+
+namespace GameOfLife.UnitTests.WebServer.Dependencies
+{
+    using System;
+    using System.Diagnostics.CodeAnalysis;
+    using GameOfLife.Basics;
+    using GameOfLife.WebServer.Dependencies;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Moq;
+
+    /// <summary>
+    /// Unit tests the ObjectFactoryBootstrapper class from the GameOfLife.WebServer.Dependencies
+    /// namespace.
+    /// </summary>
+    [TestClass, ExcludeFromCodeCoverage]
+    public class ObjectFactoryBootstrapperTests
+    {
+        /// <summary>
+        /// The Boot method should call the IObjectFactory instance provided to the constructor
+        /// and use the product as a return statement.
+        /// </summary>
+        [TestMethod]
+        public void BootCallsObjectFactoryAndResultsTheProduct()
+        {
+            // arrange
+            var expected = CreateMockGameBaseImplementation();
+
+            var mockFactory = new Mock<IObjectFactory>();
+            mockFactory
+                .Setup(x => x.Provide<IObjectFactory>(It.IsAny<object>()));
+
+            mockFactory
+                .Setup(x => x.Build<GameBase>())
+                .Returns(expected);
+
+            var bootstrapper = new ObjectFactoryBootstrapper(mockFactory.Object);
+
+            // act
+            var actual = bootstrapper.Boot<GameBase>(CreateMockRulesBaseImplementation());
+
+            // assert
+            Assert.AreSame(expected, actual, "Wrong game reference was returned from the bootstrapper.");
+        }
+
+        /// <summary>
+        /// The Boot method should catch all exceptions and rethrow them as part of a more
+        /// application specific BootFailedException.
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(
+            typeof(BootFailedException),
+            "Boot method did not throw correct exception.")]
+        public void BootThrowsBootFailedExceptionIfObjectFactoryThrowsException()
+        {
+            // arrange
+            var mockFactory = new Mock<IObjectFactory>();
+            mockFactory
+                .Setup(x => x.Provide<IObjectFactory>(It.IsAny<object>()));
+
+            mockFactory
+                .Setup(x => x.Build<GameBase>())
+                .Throws(Mock.Of<Exception>());
+
+            var bootstrapper = new ObjectFactoryBootstrapper(mockFactory.Object);
+
+            // act
+            bootstrapper.Boot<GameBase>(CreateMockRulesBaseImplementation());
+        }
+
+        private static RulesBase CreateMockRulesBaseImplementation()
+        {
+            var mockRulesBase = new Mock<RulesBase>(new int[] { }, new int[] { });
+            return mockRulesBase.Object;
+        }
+
+        private static GameBase CreateMockGameBaseImplementation()
+        {
+            var mockGameBase = new Mock<GameBase>(Mock.Of<IWriter>(), CreateMockRulesBaseImplementation());
+            return mockGameBase.Object;
+        }
+    }
+}
---
+++ GameOfLife.WebServer/Dependencies/ObjectFactoryBootstrapper.cs
@@ -0,0 +1,50 @@
+﻿// <copyright file="ObjectFactoryBootstrapper.cs" company="N/A">
+//      Copyright (C) Simon Wendel 2013-2015.
+// </copyright>
+
+namespace GameOfLife.WebServer.Dependencies
+{
+    using System;
+    using GameOfLife.Basics;
+
+    /// <summary>
+    /// Provides inversion of control by acting as both an MVC dependency resolver
+    /// and a bootstrapper.
+    /// </summary>
+    internal class ObjectFactoryBootstrapper : IBootstrapper
+    {
+        private IObjectFactory factory;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ObjectFactoryBootstrapper"/> class.
+        /// </summary>
+        /// <param name="factory">The <see cref="IObjectFactory"/> instance to use for building games.</param>
+        public ObjectFactoryBootstrapper(IObjectFactory factory)
+        {
+            this.factory = factory;
+        }
+
+        /// <summary>
+        /// Boots a new game by resolving all dependencies.
+        /// </summary>
+        /// <typeparam name="T">The type of the game to boot.</typeparam>
+        /// <param name="rules">The rules to use when running the game.</param>
+        /// <returns>A new game object.</returns>
+        public GameBase Boot<T>(RulesBase rules) where T : GameBase
+        {
+            try
+            {
+                factory.Provide<RulesBase>(rules);
+                return factory.Build<T>();
+            }
+            catch (Exception ex)
+            {
+                throw new BootFailedException(
+                    message: "Boot of the game failed.",
+                    inner: ex);
+            }
+        }
+    }
+}
+
+// eof
