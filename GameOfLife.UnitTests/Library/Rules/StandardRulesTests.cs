26b51e603badc545cbe62e4fc716fdd08b158b3e
Simon Wendel <mail@simonwendel.se>
2015-05-16 13:17:45 +0200

    Move StandardRules ctor tests into main test file

--- GameOfLife.UnitTests/Library/Rules/StandardRulesTests.cs
+++ GameOfLife.UnitTests/Library/Rules/StandardRulesTests.cs
@@ -1,10 +1,11 @@
-﻿// <copyright file="StandardRulesTests.cs" company="N/A"> 
+﻿// <copyright file="StandardRulesTests.cs" company="N/A">
 //      Copyright (C) Simon Wendel 2013-2015.
-// </copyright> 
+// </copyright>
 
 namespace GameOfLife.UnitTests.Library.Rules
 {
     using System.Diagnostics.CodeAnalysis;
+    using System.Linq;
     using GameOfLife.Library.Rules;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -12,7 +13,7 @@ namespace GameOfLife.UnitTests.Library.Rules
     /// Unit tests the standard set of rules (B3/S23) in the GameOfLife.Domain.Games namespace.
     /// </summary>
     /// <remarks>
-    /// These tests might be overkill, but it's nice to know.
+    /// These tests might be overkill, but it's nice to know that expectations are met.
     /// </remarks>
     [TestClass, ExcludeFromCodeCoverage]
     public class StandardRulesTests
@@ -36,6 +37,28 @@ namespace GameOfLife.UnitTests.Library.Rules
         }
 
         /// <summary>
+        /// When the constructor of the StandardRules class is called, the survival count and
+        /// birth count sequences are correctly initialized as per the "original" rules of
+        /// Conway's Game of Life.
+        /// </summary>
+        [TestMethod]
+        public void TheStandardRulesConstructorInitializesCorrectly()
+        {
+            // arrange
+            var birthCount = new[] { 3 };
+            var survivalCount = new[] { 2, 3 };
+
+            // assert
+            Assert.IsTrue(
+                rules.BirthCount.SequenceEqual(birthCount),
+                message: "The birth count sequences was not equal.");
+
+            Assert.IsTrue(
+                rules.SurvivalCount.SequenceEqual(survivalCount),
+                message: "The survival count sequences was not equal.");
+        }
+
+        /// <summary>
         /// Dead cell evaluated for number of neighbors n:[0->8].
         /// </summary>
         [TestMethod]
@@ -43,16 +66,16 @@ namespace GameOfLife.UnitTests.Library.Rules
         {
             // arrange
             var expectedStates = new[]
-            { 
+            {
                 DEAD,
-                DEAD, 
-                DEAD, 
-                LIVE, 
-                DEAD, 
-                DEAD, 
-                DEAD, 
-                DEAD, 
-                DEAD 
+                DEAD,
+                DEAD,
+                LIVE,
+                DEAD,
+                DEAD,
+                DEAD,
+                DEAD,
+                DEAD
             };
 
             var states = new bool[9];
@@ -81,16 +104,16 @@ namespace GameOfLife.UnitTests.Library.Rules
         {
             // arrange
             var expectedStates = new[]
-            { 
+            {
                 DEAD,
-                DEAD, 
-                LIVE, 
-                LIVE, 
-                DEAD, 
-                DEAD, 
-                DEAD, 
-                DEAD, 
-                DEAD 
+                DEAD,
+                LIVE,
+                LIVE,
+                DEAD,
+                DEAD,
+                DEAD,
+                DEAD,
+                DEAD
             };
 
             var states = new bool[9];
