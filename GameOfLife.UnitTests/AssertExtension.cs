ff1fcbae559a2f680d8f59efedc94691803e256a
Simon Wendel <mail@simonwendel.se>
2015-05-17 14:49:25 +0200

    Add the AssertExtension class with Throws<T> assertion method

---
+++ GameOfLife.UnitTests/AssertExtension.cs
@@ -0,0 +1,57 @@
+﻿// <copyright file="AssertExtension.cs" company="N/A">
+//      Copyright (C) Simon Wendel 2013-2015.
+// </copyright>
+
+namespace GameOfLife.UnitTests
+{
+    using System;
+    using System.Globalization;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    /// <summary>
+    /// Defines static extensions to the Assert class from the Microsoft
+    /// unit testing framework.
+    /// </summary>
+    public static class AssertExtension
+    {
+        /// <summary>
+        /// When passed in an action, that action should be invoked and checked for throwing
+        /// an exception of specified type.
+        /// </summary>
+        /// <typeparam name="T">The type of exception to check for.</typeparam>
+        /// <param name="action">The action to be invoked.</param>
+        /// <exception cref="AssertFailedException">
+        /// Thrown if the action does not throw an exception of type T.
+        /// </exception>
+        public static void Throws<T>(Action action) where T : Exception
+        {
+            if (action == null)
+            {
+                throw new ArgumentNullException("action");
+            }
+
+            try
+            {
+                action();
+            }
+            catch (T)
+            {
+                return;
+            }
+            catch (Exception ex)
+            {
+                var expectedType = typeof(T).GetType().Name;
+                var actualType = ex.GetType().Name;
+                var message = string.Format(
+                    CultureInfo.CurrentCulture,
+                    "Exception of type {0} expected, was of type {1}.",
+                    expectedType,
+                    actualType);
+
+                throw new AssertFailedException(message, ex);
+            }
+
+            throw new AssertFailedException("No exception thrown.");
+        }
+    }
+}
---
+++ GameOfLife.UnitTests/AssertExtensionTests.cs
@@ -0,0 +1,90 @@
+﻿// <copyright file="AssertExtensionTests.cs" company="N/A">
+//      Copyright (C) Simon Wendel 2013-2015.
+// </copyright>
+
+namespace GameOfLife.UnitTests
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    /// <summary>
+    /// Unit tests the AssertExtension class.
+    /// </summary>
+    [TestClass]
+    public class AssertExtensionTests
+    {
+        /// <summary>
+        /// When Throws{T} is given an action that does not throw an exception,
+        /// an AssertFailedException is thrown instead.
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(AssertFailedException))]
+        public void ThrowsOfT_GivenActionNotThrowingAtAll_ThrowsException()
+        {
+            // arrange
+            var obj = new object();
+
+            try
+            {
+                // act
+                AssertExtension.Throws<Exception>(() => { obj = null; });
+            }
+            catch (AssertFailedException ex)
+            {
+                // assert
+                var expectedMessage =
+                    "No exception thrown.";
+
+                Assert.AreEqual(expectedMessage, ex.Message);
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// The Throws{T} should throw AssertFailedException when the action
+        /// passed in throws an exception not of type T.
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(AssertFailedException))]
+        public void ThrowsOfT_GivenActionThrowingNotT_ThrowsException()
+        {
+            try
+            {
+                // act
+                AssertExtension.Throws<ArgumentException>(() => { throw new NotImplementedException(); });
+            }
+            catch (AssertFailedException ex)
+            {
+                // assert
+                var expectedMessage =
+                    "Exception of type ArgumentException expected, was of type NotImplementedException.";
+
+                Assert.AreEqual(expectedMessage, ex.Message);
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// When the Throws{T} method is given <value>null</value> <paramref name="action"/>
+        /// an <see cref="ArgumentNullException"/> is thrown.
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ThrowsOfT_GivenNullAction_ThrowsException()
+        {
+            // act / assert
+            AssertExtension.Throws<NotImplementedException>(null);
+        }
+
+        /// <summary>
+        /// If the action passed to Throws{T} throws the correct type of exception,
+        /// the method should quietly return.
+        /// </summary>
+        [TestMethod]
+        public void ThrowsOfT_GivenActionThrowingT_Passes()
+        {
+            // act / assert
+            AssertExtension.Throws<ArgumentException>(() => { throw new ArgumentException("somearg"); });
+        }
+    }
+}
