a1a05e7b7480040087f6101749d606724e2033ac
Simon Wendel <mail@simonwendel.se>
2015-06-13 23:07:13 +0200

    Add karma-coverage for code coverage

--- GameOfLife.FrontEnd/test/karma.conf.js
+++ GameOfLife.FrontEnd/test/karma.conf.js
@@ -4,79 +4,86 @@
 // generator-karma 1.0.0
 
 module.exports = function(config) {
-  'use strict';
+    'use strict';
 
-  config.set({
-    // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: true,
+    config.set({
+        // enable / disable watching file and executing tests whenever any file changes
+        autoWatch: true,
 
-    // base path, that will be used to resolve files and exclude
-    basePath: '../',
+        // base path, that will be used to resolve files and exclude
+        basePath: '../',
 
-    // testing framework to use (jasmine/mocha/qunit/...)
-    // as well as any additional frameworks (requirejs/chai/sinon/...)
-    frameworks: [
-      "jasmine"
-    ],
+        // testing framework to use (jasmine/mocha/qunit/...)
+        // as well as any additional frameworks (requirejs/chai/sinon/...)
+        frameworks: [
+            'jasmine'
+        ],
 
-    // list of files / patterns to load in the browser
-    files: [
-      // bower:js
-      'bower_components/modernizr/modernizr.js',
-      'bower_components/jquery/dist/jquery.js',
-      'bower_components/angular/angular.js',
-      'bower_components/angular-route/angular-route.js',
-      'bower_components/angular-sanitize/angular-sanitize.js',
-      'bower_components/bootstrap-sass-official/assets/javascripts/bootstrap.js',
-      'bower_components/jquery-ui/jquery-ui.js',
-      'bower_components/angular-mocks/angular-mocks.js',
-      // endbower
-      "app/scripts/**/*.*.js",
-      "app/scripts/**/*.js",
-      "test/mock/**/*.js",
-      "test/spec/**/*.js"
-    ],
+        // list of files / patterns to load in the browser
+        files: [
+            // bower:js
+            'bower_components/modernizr/modernizr.js',
+            'bower_components/jquery/dist/jquery.js',
+            'bower_components/angular/angular.js',
+            'bower_components/angular-route/angular-route.js',
+            'bower_components/angular-sanitize/angular-sanitize.js',
+            'bower_components/bootstrap-sass-official/assets/javascripts/bootstrap.js',
+            'bower_components/jquery-ui/jquery-ui.js',
+            'bower_components/angular-mocks/angular-mocks.js',
+            // endbower
+            'app/scripts/**/*.*.js',
+            'app/scripts/**/*.js',
+            'test/mock/**/*.js',
+            'test/spec/**/*.js'
+        ],
 
-    // list of files / patterns to exclude
-    exclude: [
-    ],
+        // list of files / patterns to exclude
+        exclude: [],
 
-    // web server port
-    port: 8080,
+        // web server port
+        port: 8080,
 
-    // Start these browsers, currently available:
-    // - Chrome
-    // - ChromeCanary
-    // - Firefox
-    // - Opera
-    // - Safari (only Mac)
-    // - PhantomJS
-    // - IE (only Windows)
-    browsers: [
-      "PhantomJS"
-    ],
+        // Start these browsers, currently available:
+        // - Chrome
+        // - ChromeCanary
+        // - Firefox
+        // - Opera
+        // - Safari (only Mac)
+        // - PhantomJS
+        // - IE (only Windows)
+        browsers: [
+            'PhantomJS'
+        ],
 
-    // Which plugins to enable
-    plugins: [
-      "karma-phantomjs-launcher",
-      "karma-jasmine"
-    ],
+        preprocessors: {
+            // source files, that you wanna generate coverage for
+            // do not include tests or libraries
+            // (these files will be instrumented by Istanbul)
+            'src/*.js': ['coverage']
+        },
 
-    // Continuous Integration mode
-    // if true, it capture browsers, run tests and exit
-    singleRun: false,
+        // Which plugins to enable
+        plugins: [
+            'karma-phantomjs-launcher',
+            'karma-jasmine',
+            'karma-coverage'
+        ],
 
-    colors: true,
+        // Continuous Integration mode
+        // if true, it capture browsers, run tests and exit
+        singleRun: false,
 
-    // level of logging
-    // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-    logLevel: config.LOG_INFO,
+        colors: true,
 
-    // Uncomment the following lines if you are using grunt's server to run the tests
-    // proxies: {
-    //   '/': 'http://localhost:9000/'
-    // },
-    // URL root prevent conflicts with the site root
-    // urlRoot: '_karma_'
-  });
+        // level of logging
+        // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
+        logLevel: config.LOG_INFO,
+
+        // Uncomment the following lines if you are using grunt's server to run the tests
+        // proxies: {
+        //   '/': 'http://localhost:9000/'
+        // },
+        // URL root prevent conflicts with the site root
+        // urlRoot: '_karma_'
+    });
 };
