79623cb9921bc36a34241113b6106dd90e390e8a
Simon Wendel <mail@simonwendel.se>
2015-06-13 23:08:12 +0200

    Add test for registering gameOfLife.app module

---
+++ GameOfLife.FrontEnd/test/spec/gameOfLife.app.js
@@ -0,0 +1,16 @@
+/*
+    <copyright file="game.js" company="N/A">
+        Copyright (C) Simon Wendel 2013-2015.
+    </copyright>
+ */
+
+'use strict';
+
+describe('Module: gameOfLife.app', function() {
+
+    it('should be registered with angular.', function() {
+        var module = angular.module('gameOfLife');
+        expect(module).toBeDefined();
+    });
+
+});
