987eaf2b3d9b00af1a28c4ebfb13d2102375f8f5
Simon Wendel <mail@simonwendel.se>
2015-06-25 21:58:34 +0200

    Add the GameHub SignalR component for running games

---
+++ GameOfLife.UnitTests/Webserver/Hubs/GameHubTests.cs
@@ -0,0 +1,198 @@
+﻿// <copyright file="GameHubTests.cs" company="N/A">
+//      Copyright (C) Simon Wendel 2013-2015.
+// </copyright>
+
+namespace GameOfLife.UnitTests.Webserver.Hubs
+{
+    using System;
+    using GameOfLife.Basics;
+    using GameOfLife.Library.Factories;
+    using GameOfLife.Library.Rules;
+    using GameOfLife.LinqLife;
+    using GameOfLife.Webserver.Hubs;
+    using GameOfLife.Webserver.Models;
+    using Microsoft.AspNet.SignalR.Hubs;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Moq;
+
+    /// <summary>
+    /// Unit tests the GameHub SignalR hub, with client side interactions.
+    /// </summary>
+    [TestClass]
+    public class GameHubTests : IDisposable
+    {
+        private Mock<IGameClientContract> clients;
+
+        private GameHub hub;
+
+        private Mock<IBootstrapper> mockBoot;
+
+        /// <summary>
+        /// Contract all game clients should implement on the receiving side.
+        /// </summary>
+        public interface IGameClientContract
+        {
+            /// <summary>
+            /// Displays an error to the user.
+            /// </summary>
+            /// <param name="error">The complete error output.</param>
+            void DisplayError(string error);
+
+            /// <summary>
+            /// Displays the results of a game run to the user.
+            /// </summary>
+            /// <param name="results">The results to render/display.</param>
+            void DisplayResults(GameBase results);
+        }
+
+        /// <summary>
+        /// Sets up the test fixture and initializes shared objects.
+        /// </summary>
+        [TestInitialize]
+        public void Setup()
+        {
+            mockBoot = new Mock<IBootstrapper>();
+
+            hub = new GameHub(mockBoot.Object);
+
+            var mockContext = new Mock<IHubCallerConnectionContext<dynamic>>();
+            hub.Clients = mockContext.Object;
+
+            clients = new Mock<IGameClientContract>();
+            mockContext.Setup(x => x.All).Returns(clients.Object);
+
+            clients.Setup(x => x.DisplayError(It.IsAny<string>())).Verifiable();
+            clients.Setup(x => x.DisplayResults(It.IsAny<GameBase>())).Verifiable();
+        }
+
+        /// <summary>
+        /// Passing a null reference for the settings to the StartGame action
+        /// will raise an exception, since this is something we can't possibly
+        /// handle in a good way.
+        /// </summary>
+        [TestMethod, ExpectedException(typeof(ArgumentNullException))]
+        public void StartGame_GivenNullGameSettings_ThrowsException()
+        {
+            // act
+            hub.StartGame(null);
+        }
+
+        /// <summary>
+        /// If the bootstrapper fails when calling the StartGame action
+        /// the hub will call displayError on the clients.
+        /// </summary>
+        [TestMethod]
+        public void StartGame_WhenBootstrappingFails_CallsErrorCallback()
+        {
+            // arrange
+            mockBoot
+                .Setup(x => x.Boot<LinqGame>(It.IsAny<RulesBase>()))
+                .Throws(new BootFailedException());
+
+            var settings = new GameSettingsModel
+            {
+                LifeForm = LifeForm.Empty,
+                NumberOfGenerations = 30,
+                Rules = Rule.Standard
+            };
+
+            // act:ish
+            hub.StartGame(settings);
+
+            // assert
+            clients.Verify(
+                x => x.DisplayError(It.IsAny<string>()),
+                Times.Once());
+        }
+
+        /// <summary>
+        /// If all goes well, the Post action returns an GameBase with state.
+        /// </summary>
+        [TestMethod]
+        public void StartGame_WhenGameRunsThrough_ReturnsGameState()
+        {
+            // arrange
+            var mockGame = BuildMockGame();
+            mockBoot
+                .Setup(x => x.Boot<LinqGame>(It.IsAny<RulesBase>()))
+                .Returns(mockGame.Object);
+
+            var settings = new GameSettingsModel
+            {
+                LifeForm = LifeForm.Empty,
+                NumberOfGenerations = 30,
+                Rules = Rule.Standard
+            };
+
+            // act
+            hub.StartGame(settings);
+
+            // assert
+            mockBoot.Verify(
+                x => x.Boot<LinqGame>(It.IsAny<RulesBase>()),
+                Times.Once());
+
+            mockGame.Verify(
+                x => x.InitializeFrom(It.IsAny<int[][]>()),
+                Times.Once());
+
+            mockGame.Verify(
+                x => x.RunThrough(It.IsAny<int>()),
+                Times.Once());
+
+            mockGame.Verify(
+                x => x.Format(),
+                Times.Never());
+
+            clients.Verify(
+                x => x.DisplayResults(It.Is<GameBase>(r => r == mockGame.Object)),
+                Times.Once());
+        }
+
+        /// <summary>
+        /// Disposes the disposable resources used by the NinjectFactory instance.
+        /// </summary>
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        /// <summary>
+        /// Disposes the disposable resources used by the NinjectFactory instance.
+        /// </summary>
+        /// <param name="disposing">
+        /// If true, the disposable resources of the NinjectFactory instance will be disposed.
+        /// </param>
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                if (hub != null)
+                {
+                    hub.Dispose();
+                    hub = null;
+                }
+            }
+        }
+
+        private static Mock<GameBase> BuildMockGame()
+        {
+            var mockGame = new Mock<GameBase>(
+                Mock.Of<IFormatter>(),
+                new StandardRules());
+
+            mockGame
+                .Setup(x => x.InitializeFrom(It.IsAny<int[][]>()));
+
+            mockGame
+                .Setup(x => x.RunThrough(It.IsAny<int>()));
+
+            mockGame
+                .Setup(x => x.Format())
+                .Returns(string.Empty);
+
+            return mockGame;
+        }
+    }
+}
---
+++ GameOfLife.Webserver/Hubs/GameHub.cs
@@ -0,0 +1,60 @@
+﻿// <copyright file="GameHub.cs" company="N/A">
+//      Copyright (C) Simon Wendel 2013-2015.
+// </copyright>
+
+namespace GameOfLife.Webserver.Hubs
+{
+    using System;
+    using GameOfLife.Basics;
+    using GameOfLife.Library.Factories;
+    using GameOfLife.LinqLife;
+    using GameOfLife.Webserver.Models;
+    using Microsoft.AspNet.SignalR;
+
+    /// <summary>
+    /// SignalR hub running games and calling back to clients on completion or error.
+    /// </summary>
+    public class GameHub : Hub
+    {
+        private IBootstrapper bootstrapper;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GameHub" /> class.
+        /// </summary>
+        /// <param name="bootstrapper">The bootstrapper used to create a game prior to running.</param>
+        public GameHub(IBootstrapper bootstrapper)
+        {
+            this.bootstrapper = bootstrapper;
+        }
+
+        /// <summary>
+        /// Starts a game and attempts to run it a specified number of generations. Will call the connected
+        /// clients on completion or error.
+        /// </summary>
+        /// <param name="settings">Settings to start the game with.</param>
+        public void StartGame(GameSettingsModel settings)
+        {
+            if (settings == null)
+            {
+                throw new ArgumentNullException("settings");
+            }
+
+            try
+            {
+                var rules = RuleFactory.Build(settings.Rules);
+                var lifeForm = LifeFormFactory.Build(settings.LifeForm);
+
+                var game = bootstrapper.Boot<LinqGame>(rules);
+                game.InitializeFrom(lifeForm.GetPattern());
+                game.RunThrough(settings.NumberOfGenerations);
+
+                Clients.All.DisplayResults(game);
+            }
+            catch (BootFailedException)
+            {
+                var message = "Booting the game failed.";
+                Clients.All.DisplayError(message);
+            }
+        }
+    }
+}
