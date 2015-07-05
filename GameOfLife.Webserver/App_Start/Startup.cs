762b337390f448b1cfc471bd40f9ebd13e47729f
Simon Wendel <mail@simonwendel.se>
2015-07-05 16:26:14 +0200

    Add an OWIN startup class configuring SignalR

---
+++ GameOfLife.Webserver/App_Start/Startup.cs
@@ -0,0 +1,34 @@
+﻿// <copyright file="Startup.cs" company="N/A">
+//      Copyright (C) Simon Wendel 2013-2015.
+// </copyright>
+
+[assembly: Microsoft.Owin.OwinStartup(typeof(GameOfLife.Webserver.App_Start.Startup))]
+
+namespace GameOfLife.Webserver.App_Start
+{
+    using GameOfLife.Webserver.Dependencies;
+    using Microsoft.AspNet.SignalR;
+    using Ninject;
+    using Owin;
+
+    /// <summary>
+    /// OWIN Startup class.
+    /// </summary>
+    public class Startup
+    {
+        /// <summary>
+        /// Configures an OWIN application.
+        /// </summary>
+        /// <param name="application"><see cref="IAppBuilder" /> to configure.</param>
+        public void Configuration(IAppBuilder application)
+        {
+            var kernel = new StandardKernel();
+            var resolver = new NinjectDependencyResolver(kernel);
+
+            var config = new HubConfiguration();
+            config.Resolver = resolver;
+
+            application.MapSignalR(config);
+        }
+    }
+}
