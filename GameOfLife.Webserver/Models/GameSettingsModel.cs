2f90476b5027470fb94d2bae268593f6de0bf3d9
Simon Wendel <mail@simonwendel.se>
2015-07-05 19:19:59 +0200

    Remove old Resource strings from web project

--- GameOfLife.Webserver/Models/GameSettingsModel.cs
+++ GameOfLife.Webserver/Models/GameSettingsModel.cs
@@ -4,7 +4,6 @@
 
 namespace GameOfLife.Webserver.Models
 {
-    using System.ComponentModel.DataAnnotations;
     using GameOfLife.Library.Factories;
 
     /// <summary>
@@ -15,37 +14,16 @@ namespace GameOfLife.Webserver.Models
         /// <summary>
         /// Gets or sets the number of generations to run the game.
         /// </summary>
-        [Display(
-            Name = "GameSettingsModel_NumberOfGenerations_Name",
-            Description = "GameSettingsModel_NumberOfGenerations_Description",
-            ResourceType = typeof(Resources.ViewModelResources))]
-        [Required(
-            ErrorMessageResourceName = "GameSettingsModel_NumberOfGenerations_Missing",
-            ErrorMessageResourceType = typeof(Resources.ViewModelResources))]
         public int NumberOfGenerations { get; set; }
 
         /// <summary>
         /// Gets or sets the rules governing the game, defaults to Standard.
         /// </summary>
-        [Display(
-            Name = "GameSettingsModel_Rules_Name",
-            Description = "GameSettingsModel_Rules_Description",
-            ResourceType = typeof(Resources.ViewModelResources))]
-        [Required(
-            ErrorMessageResourceName = "GameSettingsModel_Rules_Missing",
-            ErrorMessageResourceType = typeof(Resources.ViewModelResources))]
         public Rule Rules { get; set; }
 
         /// <summary>
-        /// Gets or sets the initial life form pattern, defaults to a random grid.
+        /// Gets or sets the initial life form pattern.
         /// </summary>
-        [Display(
-            Name = "GameSettingsModel_LifeForm_Name",
-            Description = "GameSettingsModel_LifeForm_Description",
-            ResourceType = typeof(Resources.ViewModelResources))]
-        [Required(
-            ErrorMessageResourceName = "GameSettingsModel_LifeForm_Missing",
-            ErrorMessageResourceType = typeof(Resources.ViewModelResources))]
         public LifeForm LifeForm { get; set; }
     }
 }
