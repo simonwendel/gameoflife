c36e310aa319fc8c2d741c9ae2b2ea8301fcdf17
Simon Wendel <mail@simonwendel.se>
2015-08-08 23:48:14 +0200

    Add test to see if startDialog module is defined

---
+++ GameOfLife.FrontEnd/test/spec/modules/startDialog/startDialog.module.js
@@ -0,0 +1,17 @@
+/*
+    <copyright file="startDialog.module.js" company="N/A">
+        Copyright (C) Simon Wendel 2013-2015.
+    </copyright>
+ */
+
+;(function() {
+    'use strict';
+
+    describe('Module: gameOfLife.startDialog', function() {
+
+        it('should be defined and accessible.', function() {
+            expect(angular.module('gameOfLife.startDialog')).toBeDefined();
+        });
+
+    });
+})();
