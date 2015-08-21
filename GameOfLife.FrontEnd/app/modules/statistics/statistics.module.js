9c1ac5f0dedc6056ee8d5714a9531758bdaa0785
Simon Wendel <mail@simonwendel.se>
2015-08-21 15:17:19 +0200

    Add the statistics module

---
+++ GameOfLife.FrontEnd/app/modules/statistics/statistics.module.js
@@ -0,0 +1,12 @@
+/*
+    <copyright file="statistics.module.js" company="N/A">
+        Copyright (C) Simon Wendel 2013-2015.
+    </copyright>
+*/
+
+;(function() {
+    'use strict';
+
+    angular
+        .module('gameOfLife.statistics', []);
+})();
---
+++ GameOfLife.FrontEnd/test/spec/modules/statistics/statistics.module.js
@@ -0,0 +1,17 @@
+/*
+    <copyright file="statistics.module.js" company="N/A">
+        Copyright (C) Simon Wendel 2013-2015.
+    </copyright>
+*/
+
+;(function() {
+    'use strict';
+
+    describe('Module: gameOfLife.statistics', function() {
+
+        it('should be defined and accessible.', function() {
+            expect(angular.module('gameOfLife.statistics')).toBeDefined();
+        });
+
+    });
+})();
